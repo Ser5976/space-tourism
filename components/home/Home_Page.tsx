de7d95b583f3c90a5485f9d1af6561b361160f0d
Seriy <tsbaranova@bk.ru>
2023-07-05 21:43:53 +0300

    test3

--- components/home/Home_Page.tsx
+++ components/home/Home_Page.tsx
@@ -1,8 +1,30 @@
 import { bellefair, barlowCondensed } from '@/fonts/fonts';
+import Image from 'next/image';
 import styles from './Home_Page.module.css';
 
 const HomePage = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <>
+      <Image
+        src="/home/desktop.jpg"
+        alt="фоновая картинка"
+        fill
+        className={styles.desktop_image}
+      />
+      <Image
+        src="/home/tablet.jpg"
+        alt="фоновая картинка"
+        fill
+        className={styles.tablet_image}
+      />
+      <Image
+        src="/home/mobile.jpg"
+        alt="фоновая картинка"
+        fill
+        className={styles.mobile_image}
+      />
+    </>
+  );
 };
 
 export default HomePage;
